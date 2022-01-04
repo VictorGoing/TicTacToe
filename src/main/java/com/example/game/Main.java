4f15fee77795e21b27c217e6961b3c1ac1c67e4f
WiktorIdziak <Witusaki@gmail.com>
2022-01-04 11:13:21 +0100

    Initial commit

---
+++ src/main/java/com/example/game/Main.java
@@ -0,0 +1,35 @@
+package com.example.game;
+
+import javafx.application.Application;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    private Image imageback = new Image("file:Background.png");
+
+    @Override
+    public void start(Stage primaryStage) {
+
+        BackgroundSize backgroundSize = new BackgroundSize(100, 100, true, true, true, false);
+        BackgroundImage backgroundImage = new BackgroundImage(imageback, BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, backgroundSize);
+        Background background = new Background(backgroundImage);
+
+        GridPane grid = new GridPane();
+        grid.setBackground(background);
+
+        Scene scene = new Scene(grid, 800, 600, Color.BLACK);
+
+        primaryStage.setTitle("TicTacToe");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
